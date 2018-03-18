6374d2b61806d00964c6080670aec7e4570a18e2
Eric White <eric.white@gmail.com>
2018-03-17 22:10:35 -0700

    Set init script to install requirements for ansible and download git repo

--- init.sh
+++ init.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-
+sudo apt-get update
+sudo apt-get install -y git python-pip python-dev
+sudo pip install ansible
+cd /tmp
+git clone https://github.com/EricWayneWhite/ansible-workstation.git
+cd ansible-workstation
+sed -i "s/git@github.com:/https:\/\/github.com\//" .gitmodules
+git submodule init
+git submodule update
+ansible-playbook main.yml -b -K -c local -i "localhost,"
